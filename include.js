4ad98313c1d266ece9745be30faa3226f18786d0
mfo13 <128325284+mfo13@users.noreply.github.com>
2023-04-26 19:37:55 -0300

    Add files via upload

---
+++ include.js
@@ -0,0 +1,11 @@
+function include(thing) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(thing).innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", thing, true);
+  xhttp.send();
+};

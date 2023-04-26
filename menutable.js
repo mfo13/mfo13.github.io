8a6b0e5743b552dc1ac1133830fcc66abb3b1f65
mfo13 <128325284+mfo13@users.noreply.github.com>
2023-04-26 17:27:08 -0300

    Add files via upload

---
+++ menutable.js
@@ -0,0 +1,16 @@
+function menutable() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("menutable").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "navbar.xml", true);
+  xhttp.send();
+
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) 
+  {
+    navbar();
+  }
+};
---
+++ navbar.js
@@ -0,0 +1,11 @@
+function navbar() {
+  var width = '100%';
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    width = (screen.width+200).toString()+'px';
+  }
+  document.getElementById('menuicon').classList.toggle("change");
+  document.getElementById('menu').style.display = document.getElementById('menu').style.display=='none' ? 'block' : 'none';
+  document.getElementById('menutable').style.background = document.getElementById('menu').style.display=='none' ? 'white' : 'black';
+  document.getElementById('menutable').style.width = document.getElementById('menu').style.display=='none' ? '25px' : '150px';
+  document.getElementById('maintable').style.width = document.getElementById('menu').style.display=='none' ? '100%' : width;
+}

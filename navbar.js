1d66a333c16da4afa122e5801c00912a57ff06db
mfo13 <128325284+mfo13@users.noreply.github.com>
2023-04-27 00:56:59 -0300

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -4,8 +4,9 @@ function navbar() {
     width = (screen.width+200).toString()+'px';
   }
   document.getElementById('menuicon').classList.toggle("change");
+  document.getElementById('menuicon').innerHTML = document.getElementById('menuicon').innerHTML=='x' ? '+' : 'x';
   document.getElementById('menu').style.display = document.getElementById('menu').style.display=='none' ? 'block' : 'none';
   document.getElementById('menutable').style.background = document.getElementById('menu').style.display=='none' ? 'white' : 'black';
-  document.getElementById('menutable').style.width = document.getElementById('menu').style.display=='none' ? '25px' : '150px';
+  document.getElementById('menutable').style.width = document.getElementById('menu').style.display=='none' ? '0px' : '200px';
   document.getElementById('maintable').style.width = document.getElementById('menu').style.display=='none' ? '100%' : width;
 }
